e30f93a8d87b7d22b66a48562adff49a759cf126
umar <umar2752018@gmail.com>
2025-10-25 19:11:03 +0300

    Corrected

--- components/Education.js
+++ components/Education.js
@@ -3,6 +3,11 @@ export default function Education() {
     <section id="education">
       <h2>Education</h2>
       <div>
+        <h3>Educational Professional Development Program for Private School Teachers</h3>
+        <p>University of Bahrain (November 2024)</p>
+        <p>Completed training in Educational Psychology, Classroom Management, Teaching Strategies, Lesson Planning, and Assessment & Evaluation</p>
+      </div>
+      <div>
         <h3>Master of Computer Application</h3>
         <p>University of Calicut (Graduated: December 2024)</p>
         <p>Specialized in Computer Science with core papers in Programming, Computer Architecture, Accounting, and Mathematics.</p>
@@ -12,11 +17,7 @@ export default function Education() {
         <p>University of Calicut (Graduated: April 2011)</p>
         <p>Specialized in Computer Science with Mathematics and Statistics as subsidiary subjects</p>
       </div>
-      <div>
-        <h3>Educational Professional Development Program for Private School Teachers</h3>
-        <p>University of Bahrain (November 2024)</p>
-        <p>Completed training in Educational Psychology, Classroom Management, Teaching Strategies, Lesson Planning, and Assessment & Evaluation</p>
-      </div>
+      
     </section>
   );
 }
