56e513b27a44295d1002ee0ea754113f1a894193
umar <umar2752018@gmail.com>
2025-10-25 18:43:13 +0300

    Sectionwise content

--- components/Downloads.js
+++ components/Downloads.js
@@ -29,33 +29,34 @@ export default function Downloads() {
       <h1>Download Page</h1>
       <p>Hello! I am [Your Name], an enthusiastic teacher...</p>
 
+      {/* PowerPoints Section */}
       <div style={{ marginTop: '2rem' }}>
-        <h2>IGCSE Computer Science - PowerPoint Presentations</h2>
+        <h2>PowerPoints</h2>
 
-        {/* Past paper Question 1.1 */}
         <div style={containerStyle}>
-          <span style={titleStyle}>1.1 – Number systems</span>
-          <a href="/pastpapersquestions/1.1.pdf" download style={buttonStyle}>
-            Download
-          </a>
-        </div>
-
-
-
-        {/* Presentation 4.1 */}
-        <div style={containerStyle}>
-          <span style={titleStyle}>4.1 – Types of software and interrupts</span>
+          <span style={titleStyle}>4.1 – Types of Software and Interrupts</span>
           <a href="/powerpoints/4.1.pptx" download style={buttonStyle}>
             Download
           </a>
         </div>
 
-        {/* Presentation 4.2 */}
         <div style={containerStyle}>
           <span style={titleStyle}>
             4.2 – Types of Programming Languages, Translators, IDEs
           </span>
           <a href="/powerpoints/4.2.pptx" download style={buttonStyle}>
+            Download
+          </a>
+        </div>
+      </div>
+
+      {/* Past Paper Questions Section */}
+      <div style={{ marginTop: '3rem' }}>
+        <h2>Past Paper Questions</h2>
+
+        <div style={containerStyle}>
+          <span style={titleStyle}>1.1 – Number Systems</span>
+          <a href="/pastpapersquestions/1.1.pdf" download style={buttonStyle}>
             Download
           </a>
         </div>
