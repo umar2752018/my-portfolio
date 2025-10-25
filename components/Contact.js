a6715fe5a806f863acbf0e70e358fcdfe8e154cf
umar <umar2752018@gmail.com>
2025-10-25 19:22:19 +0300

    Contact now in the middle

--- components/Contact.js
+++ components/Contact.js
@@ -2,13 +2,16 @@ export default function Contact() {
   return (
     <section id="contact">
       <h2>Contact</h2>
+      <center>Get in touch with me via the following contact details:
       <ul>
+        
         <li>Email: <a href="mailto:umar2752018@gmail.com">uamr2752018@gmail.com</a></li>
         <li>Phone: +91-xxxxxxxxx (India)</li>
         <li>Phone: +973-xxxxxxxxxx (Bahrain)</li>
         <li>Location: Kerala, India</li>
         <li>LinkedIn: <a href="https://linkedin.com/in/muqthar91" target="_blank">muqthar91</a></li>
       </ul>
+      </center>
     </section>
   );
 }
